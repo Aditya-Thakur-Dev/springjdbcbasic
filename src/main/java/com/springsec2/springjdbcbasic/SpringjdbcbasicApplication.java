b1caac59c307394be54d7b9e47450bc8c0147bdb
Aditya Thakur <atdthk@gmail.com>
2021-04-27 19:35:41 +0530

    First commit

---
+++ src/main/java/com/springsec2/springjdbcbasic/SpringjdbcbasicApplication.java
@@ -0,0 +1,13 @@
+package com.springsec2.springjdbcbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjdbcbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjdbcbasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsec2/springjdbcbasic/SpringjdbcbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsec2.springjdbcbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjdbcbasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
